249af1a1e5a59064aa9f13e3a307c34f5c546937
ramonm2811 <ramonm2811@gmail.com>
2021-09-18 18:08:49 +0000

    primer commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let potencia = document.getElementById("potencia").value;
+let distancia = document.getElementById("distancia").value;
+let kz1 = document.getElementById("kz1");
+let kz2 = document.getElementById("kz2");
+let cdt = document.getElementById("cdt");
+let constCorreccion = document.getElementById("constCorreccion");
+let valorTotal = document.getElementById("valorTotal");
+
+console.log();
+
+const CDT = () => {
+  let auxCdt = potencia * distancia;
+};
